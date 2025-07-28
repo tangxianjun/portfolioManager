b7744b39ccc6e111fad740b08ba7caa6418ca31f
Jerry Liu <1745396756@qq.com>
2025-07-28 09:44:46 +0000

    stock change

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/stock', function(req, res, next) {
+  res.render('stock', { title: 'Stock management' });
+});
 module.exports = router;
