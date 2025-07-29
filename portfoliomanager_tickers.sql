7fe79fbad7247f5bdc785f38aa44a0c7cdc57ec9
tristan <1125288167@qq.com>
2025-07-29 07:33:02 +0000

    db

--- portfoliomanager_tickers.sql
+++ portfoliomanager_tickers.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `portfoliomanager` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `portfoliomanager`;
 -- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
 --
 -- Host: localhost    Database: portfoliomanager
@@ -56,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-28  9:35:48
+-- Dump completed on 2025-07-29  7:32:39
