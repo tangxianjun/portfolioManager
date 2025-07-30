56865bdee9db8ad29cdad52f1c8861123c4ac613
tristan <1125288167@qq.com>
2025-07-30 03:18:46 +0000

    add_wealth

--- service/myWealth.js
+++ service/myWealth.js
@@ -1,10 +1,16 @@
 var pool = require('../config/db');
 
 const myWealth = async () => {
-    const query = 'SELECT * FROM wealth LEFT JOIN com_icon ON wealth.code = com_icon.code WHERE wealth.share > 0';
-
+    var query = 'SELECT * FROM wealth LEFT JOIN com_icon ON wealth.code = com_icon.code WHERE wealth.share > 0';
     try {
-        const [rows] = await pool.query(query);
+        var [rows] = await pool.query(query);
+        for (var i = 0; i < rows.length; i++) {
+            query = 'SELECT * FROM tickers WHERE ticker = ? ORDER BY t DESC LIMIT 1';
+            const [tickerRows] = await pool.query(query, [rows[i]['code']]);
+            rows[i]["current_price"] = tickerRows.length > 0 ? tickerRows[0]['vw'] : null;
+            rows[i]["value"] = rows[i]['share'] * rows[i]['avg_cost'];
+            rows[i]['banlance'] = rows[i]['share'] * (rows[i]['current_price'] - rows[i]['avg_cost']);
+        }
         console.log(rows);
         if (rows.length > 0) {
             return rows;
@@ -16,5 +22,5 @@ const myWealth = async () => {
         throw error;
     }
 }
-
+// myWealth();
 module.exports = myWealth;
